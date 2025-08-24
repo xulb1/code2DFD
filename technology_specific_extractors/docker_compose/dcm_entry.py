78f79a318ce5e80ea1373b7d8cf95c4c9be061ad
xulb1 <delestjo54@gmail.com>
2025-08-24 21:33:14 +0200

    fix: pb with tagged_value for port

--- technology_specific_extractors/docker_compose/dcm_entry.py
+++ technology_specific_extractors/docker_compose/dcm_entry.py
@@ -80,7 +80,13 @@ def dictionarify(elements_set: set, properties_dict: dict) -> dict:
             stereotypes = []
             tagged_values = []
         if e[3]:
-            tagged_values.append(("Port", str(list(e[3][0]))))
+            try:
+                tagged_values.append(("Port", str(list(e[3][0]))))
+            except TypeError:
+                tagged_values.append(("Port", str(e[3][0])))
+            except:
+                print(list(e[3]))
+                
             traceability.add_trace({
                 "parent_item": e[0],#.replace("pom_", "")
                 "item": "Port",
